f09b630d78369805c375d63668f330709a95adf2
Zhang Kaiyi <kzoacn@outlook.com>
2020-03-02 18:24:57 +0800

    Add files via upload

---
+++ NovelGen/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace NovelGen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.另存为ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.打开ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.另存为ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.fromA = new System.Windows.Forms.TextBox();
+            this.toB = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(714, 139);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(487, 420);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            this.listBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.listBox1_KeyPress);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(714, 73);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(104, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(903, 73);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "随机起点";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(399, 52);
+            this.label1.MaximumSize = new System.Drawing.Size(300, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "选择文本显示";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(30, 52);
+            this.label2.MaximumSize = new System.Drawing.Size(300, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "文本最后五行";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.另存为ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1233, 25);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 另存为ToolStripMenuItem
+            // 
+            this.另存为ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.打开ToolStripMenuItem,
+            this.另存为ToolStripMenuItem1,
+            this.退出ToolStripMenuItem});
+            this.另存为ToolStripMenuItem.Name = "另存为ToolStripMenuItem";
+            this.另存为ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.另存为ToolStripMenuItem.Text = "菜单";
+            // 
+            // 打开ToolStripMenuItem
+            // 
+            this.打开ToolStripMenuItem.Name = "打开ToolStripMenuItem";
+            this.打开ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.打开ToolStripMenuItem.Text = "打开";
+            this.打开ToolStripMenuItem.Click += new System.EventHandler(this.打开ToolStripMenuItem_Click);
+            // 
+            // 另存为ToolStripMenuItem1
+            // 
+            this.另存为ToolStripMenuItem1.Name = "另存为ToolStripMenuItem1";
+            this.另存为ToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.另存为ToolStripMenuItem1.Text = "另存为";
+            this.另存为ToolStripMenuItem1.Click += new System.EventHandler(this.另存为ToolStripMenuItem1_Click);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.退出ToolStripMenuItem.Text = "退出";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(714, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(903, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 21);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "分析文本";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(1002, 39);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(190, 35);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(903, 106);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(71, 27);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "删除最后";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 12;
+            this.listBox2.Location = new System.Drawing.Point(716, 612);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(176, 136);
+            this.listBox2.TabIndex = 10;
+            // 
+            // fromA
+            // 
+            this.fromA.Location = new System.Drawing.Point(926, 615);
+            this.fromA.Name = "fromA";
+            this.fromA.Size = new System.Drawing.Size(76, 21);
+            this.fromA.TabIndex = 11;
+            // 
+            // toB
+            // 
+            this.toB.Location = new System.Drawing.Point(1051, 612);
+            this.toB.Name = "toB";
+            this.toB.Size = new System.Drawing.Size(79, 21);
+            this.toB.TabIndex = 12;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(929, 655);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(72, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "添加置换";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1051, 655);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(72, 23);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "删除";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(838, 74);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(54, 20);
+            this.button6.TabIndex = 15;
+            this.button6.Text = "自定义";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(1006, 98);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(185, 34);
+            this.button7.TabIndex = 16;
+            this.button7.Text = "复制文本";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1233, 775);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.toB);
+            this.Controls.Add(this.fromA);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 另存为ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 另存为ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 打开ToolStripMenuItem;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.TextBox fromA;
+        private System.Windows.Forms.TextBox toB;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ NovelGen/Form1.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.IO;
+using JiebaNet;
+using JiebaNet.Segmenter.PosSeg;
+
+namespace NovelGen
+{
+    public partial class Form1 : Form
+    {
+
+        private ArrayList text = new ArrayList();
+        private ArrayList inds = new ArrayList();
+        private ArrayList vec = new ArrayList();
+        private string[] lines;
+        private int[] tK;
+        private PosSegmenter posSeg;
+        private Dictionary<string,int> keyWords = new Dictionary<string,int>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var rng = new Random();
+            int st= rng.Next(0, lines.Length-1);
+            text = new ArrayList();
+            inds = new ArrayList();
+            inds.Add(st);
+            text.Add(modify(lines[st]));
+
+            label2.Text = lines[st];
+
+            refresh();
+    }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label1.Text = (String)listBox1.SelectedItem;
+        }
+
+        private string modify(string s)
+        {
+
+            foreach(var p in listBox2.Items)
+            {
+                string a = ((KeyValuePair<string, string>)p).Key;
+                string b = ((KeyValuePair<string, string>)p).Value;
+                s=s.Replace(a, b);
+            }
+            return s;
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+
+            int chs = listBox1.SelectedIndex;
+            text.Add(modify(lines[tK[chs] + 1]));
+            inds.Add(tK[chs] + 1);
+
+
+
+
+            refresh();
+        }
+
+        private void 另存为ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            string localFilePath = ""; 
+            SaveFileDialog sfd = new SaveFileDialog(); 
+            sfd.Filter = "文本文件|*.txt"; 
+            sfd.FilterIndex = 1; 
+            sfd.RestoreDirectory = true; 
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                localFilePath = sfd.FileName.ToString();  
+                File.AppendAllLines(localFilePath, text.Cast<string>());
+            }
+
+        }
+
+        private void 打开ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Multiselect = true;
+            fileDialog.Title = "请选择文件";
+            fileDialog.Filter = "文本文件|*.txt*"; 
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string file = fileDialog.FileName;     
+                label3.Text = file;
+            }
+        }
+
+        public class KeyCompare : IComparer
+        {
+            public int Compare(object manA, object manB)
+            {
+                var man1 = (KeyValuePair<int, string>)manA;
+                var man2 = (KeyValuePair<int, string>)manB;
+                return man1.Key.CompareTo(man2.Key);
+            }
+        }
+        public class KeyCompare2 : IComparer
+        {
+            public int Compare(object manA, object manB)
+            {
+                var man1 = (KeyValuePair<double, int>)manA;
+                var man2 = (KeyValuePair<double, int>)manB;
+                return man1.Key.CompareTo(man2.Key);
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+         
+
+            lines = File.ReadAllLines(label3.Text);
+            
+           
+
+            var dic = new Dictionary<string, int>();
+            int all = lines.Length;
+            int cnt = 0;
+            posSeg = new PosSegmenter();
+            foreach (var l in lines){
+                cnt++;
+                progressBar1.Value = 50*cnt / all;
+                var tokens = posSeg.Cut(l);
+                foreach(var p in tokens)
+                {
+                    if (p.Flag != "n")
+                        continue;
+                    if (dic.ContainsKey(p.Word))
+                    {
+                        dic[p.Word] += 1;
+                    }
+                    else
+                    {
+                        dic[p.Word] = 1;
+                    }
+                }
+            }
+
+            var arr = new ArrayList();
+            foreach(var p in dic)
+            {
+                arr.Add(new KeyValuePair<int,string>(p.Value, p.Key));
+            }
+
+            arr.Sort(new KeyCompare());
+            arr.Reverse();
+            keyWords = new Dictionary<string, int>();
+            for(int i = 0; i < 100; i++)
+            {
+                var p = (KeyValuePair<int, string>)arr[i];
+                keyWords[p.Value] = 1;
+                 
+            }
+
+            vec = new ArrayList();
+            var last = new Dictionary<string, int>();
+            foreach(var p in keyWords)
+            {
+                last[p.Key] = -1000;
+            }
+            for(int i = 0; i < lines.Length; i++)
+            {
+                progressBar1.Value = 50 + 50 * (i+1) / all;
+                var tokens = posSeg.Cut(lines[i]);
+                foreach(var p in tokens)
+                {
+                    if (keyWords.ContainsKey(p.Word))
+                    {
+                        last[p.Word] = i;
+                    }
+                }
+                double s = 0;
+                double[] v = new double[keyWords.Count];
+                int j = 0;
+                foreach(var p in keyWords)
+                {
+                    double t = 1.0 / (i - last[p.Key] + 1);
+                    s += t * t;
+                    v[j] = t;
+                    j++;
+                }
+                s = Math.Sqrt(s);
+                for (int k = 0; k < v.Length; k++)
+                    v[k] /= s;
+                vec.Add(v);
+            }
+            button1_Click(null, null);
+        }
+        private int[] topK(int ind,int k = 8)
+        {
+            int[] res = new int[k+1];
+            int gap = 15;
+            var arr = new ArrayList(); 
+                
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (ind - gap < i && i < ind + gap)
+                    continue;
+
+                double d=0;
+                double[] v1 = (double[])vec[i];
+                double[] v2 = (double[])vec[ind];
+                for (int j = 0; j < v1.Length; j++)
+                    d += v1[j] * v2[j]; 
+                arr.Add(new KeyValuePair<double, int>(d, i));
+            }
+            arr.Sort(new KeyCompare2());
+            arr.Reverse();
+            res[0] = ind;
+            for (int i = 0; i < k; i++)
+                res[i+1] = ((KeyValuePair<double, int>)arr[i]).Value;
+            return res;
+        }
+
+        private void refresh()
+        {
+            int len = 1;
+            label2.Text = "";
+            int s = 0;
+            for(int i = 1; i <= 5; i++)
+            {
+                if (text.Count - i < 0)
+                    continue;
+                s += ((string)text[text.Count - i]).Length;
+                if (s < 400)
+                {
+                    len++;
+                }
+            }
+            for (int i = len; i >= 1; i--)
+            {
+                if (text.Count - i < 0)
+                    continue;
+                label2.Text += text[text.Count - i];
+                label2.Text += "\n";
+            }
+
+            int cur = (int)inds[inds.Count - 1];
+            tK = topK(cur);
+            listBox1.Items.Clear();
+            for(int i = 0; i < tK.Length; i++)
+            {
+                listBox1.Items.Add(lines[tK[i]+1]);
+            }
+
+            listBox1.SetSelected(0, true);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            text.RemoveAt(text.Count - 1);
+            inds.RemoveAt(inds.Count - 1);
+            refresh();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string a = fromA.Text;
+            string b = toB.Text;
+
+            listBox2.Items.Add(new KeyValuePair<string, string>(a, b));
+                
+         }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if(listBox2.SelectedItem!=null)
+                listBox2.Items.Remove(listBox2.SelectedItem);
+        }
+
+        private void listBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+            if (e.KeyChar == (char)13)
+            {
+                listBox1_DoubleClick(null, null);
+                return;
+            }
+            if (e.KeyChar==(char)Keys.Back)
+            {
+
+
+                button3_Click(null, null);
+                return;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            
+            int st = int.Parse(textBox1.Text);
+            if (st < 0 || st >= lines.Length)
+                return;
+            text = new ArrayList();
+            inds = new ArrayList();
+            inds.Add(st);
+            text.Add(modify(lines[st]));
+
+            label2.Text = lines[st];
+
+            refresh();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string all = "";
+            foreach(var s in text)
+            {
+                all += s;
+                all += "\n";
+            }
+            Clipboard.SetText(all);
+        }
+    }
+}
